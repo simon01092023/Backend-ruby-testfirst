a71ba14da37e81d4c14cd1419693d378afc31052
simon01092024 <simonkudus510@gmail.com>
2024-07-16 17:16:06 -0700

    lesson 5 commit

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(input)
+  input
+end
+
+def shout(input)
+  input.upcase
+end
+
+def repeat(input, times = 2)
+  Array.new(times, input).join(' ')
+end
+
+def start_of_word(input, num)
+  input[0, num]
+end
+
+def first_word(input)
+  input.split.first
+end
+
+def titleize(input)
+  little_words = %w[and over the the]
+  words = input.split.map.with_index do |word, index|
+    if index.zero? || !little_words.include?(word)
+      word.capitalize
+    else
+      word
+    end
+  end
+  words.join(' ')
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title=(new_title)
+    @title = capitalize_title(new_title)
+  end
+
+  private
+
+  def capitalize_title(title)
+    little_words = %w[and the a an in of]
+    words = title.split(' ')
+    capitalized_title = []
+
+    words.each_with_index do |word, index|
+      capitalized_title << if index.zero? || !little_words.include?(word.downcase)
+                             word.capitalize
+                           else
+                             word
+                           end
+    end
+
+    capitalized_title.join(' ')
+  end
 end
